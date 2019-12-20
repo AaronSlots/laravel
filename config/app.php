e217a815661d95ca33997dcfe07cf4ec8e8fb493
AaronSlots <aaronslots@gmail.com>
2019-12-20 10:33:52 +0100

    update

---
+++ config/app.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'providers' => ASSoftware\Laravel\LaravelServiceProvider::class,
+];
--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -4,12 +4,28 @@ use Illuminate\Support\ServiceProvider;
 class LaravelServiceProvider extends ServiceProvider
 {
     /**
+    * Make config publishment optional by merging the config from the package.
+    *
+    * @return  void
+    */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/app.php',
+            'app'
+        );
+    }
+    
+    /**
     * Publishes configuration file.
     *
     * @return  void
     */
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/../config/app.php' => config_path('app.php'),
+        ]);
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         #$this->loadViewsFrom(__DIR__.'/resources/views','as-software/laravel');
         $this->publishes([
