5d6494aab94d02c30ca1149bea42397ccb0bfc8c
AaronSlots <aaronslots@gmail.com>
2019-12-21 15:49:36 +0100

    Update LaravelServiceProvider.php

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -26,8 +26,8 @@ class LaravelServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         #$this->loadViewsFrom(__DIR__.'/resources/views','as-software/laravel');
         $this->publishes([
-            __DIR__.'/app' => app_path()
+            __DIR__.'/app' => app_path(),
+            __DIR__.'/routes' => base_path('routes/web.php')
         ]);
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
     }
 }
